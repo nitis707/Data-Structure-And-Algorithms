6125ddc760011d85b93da751c4aa855bfae19075
Nitish Singh <nitishkumarsingh909@gmail.com>
2023-08-28 16:25:04 +0530

    input

---
+++ 02 Write your First C++ Program/takeInput.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int marks;
+
+    cout << "Enter the marks here: ";
+    cin >> marks;
+
+    cout << "Printing marks: " << marks << endl;
+    return 0;
+}
--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # Data-Structure-And-Algorithms
+<br>
+cd CodeHelp_DSA/Data-Structure-And-Algorithms
+<br>
 This is my DSA repository.
 <br>
 Author - Nitish Singh
+
