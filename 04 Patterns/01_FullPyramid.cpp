73e413d7e393ee7da5092f0ebdbe0b81c5d1866c
Nitish Singh <nitishkumarsingh909@gmail.com>
2023-09-05 10:19:07 +0530

    Upload

---
+++ 04 Patterns/01_FullPyramid.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter N: ";
+    cin >> n;
+
+    for (int row = 0; row < n; row++)
+    {
+        // Inner Loop Space
+        for (int col = 0; col < n - row - 1; col++)
+        {
+            cout << " ";
+        }
+        // Inner Loop Stars
+        for (int col = 0; col < row + 1; col++)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
+
+/* Output:
+Enter N: 10
+         *
+        * *
+       * * *
+      * * * *
+     * * * * *
+    * * * * * *
+   * * * * * * *
+  * * * * * * * *
+ * * * * * * * * *
+* * * * * * * * * *
+*/
---
+++ 04 Patterns/02_InvertedFullPyramid.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter N: ";
+    cin >> n;
+
+    for (int row = 0; row < n; row++)
+    {
+        // Inner for Spaces
+        for (int col = 0; col < row; col++)
+        {
+            cout << " ";
+        }
+        // Inner for Stars
+        for (int col = 0; col < n - row; col++)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
