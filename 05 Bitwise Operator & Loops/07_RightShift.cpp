333e95b4dfe9a1ac88bc31777c63b52656fad7d9
Nitish Singh <nitishkumarsingh909@gmail.com>
2023-09-10 22:35:07 +0530

    Upload

---
+++ 05 Bitwise Operator & Loops/07_RightShift.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n = 5;
+    int ans = (n >> 1);
+    cout <<"RightShift of 5 is: "<< ans;
+
+    return 0;
+}
+
+/* Output:
+RightShift of 5 is: 2
+*/
