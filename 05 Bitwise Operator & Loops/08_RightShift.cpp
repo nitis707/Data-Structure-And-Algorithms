c158c42f30837f3948eb24898d21cda4c98ec0fd
Nitish Singh <nitishkumarsingh909@gmail.com>
2023-09-10 22:38:46 +0530

    Upload

--- 05 Bitwise Operator & Loops/08_RightShift.cpp
+++ 05 Bitwise Operator & Loops/08_RightShift.cpp
@@ -1,8 +1,14 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 int main()
 {
-    
+    int n = 50;
+    int ans = (n >> 2);
+    cout << "RightShift by 2 is: " << ans << endl;
     return 0;
 }
+
+/* Output:
+RightShift by 2 is: 12
+*/
