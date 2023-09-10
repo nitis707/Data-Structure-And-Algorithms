9c5c0ec4681e6b93a84c76dd7f5e738eea7baa64
Nitish Singh <nitishkumarsingh909@gmail.com>
2023-09-10 16:03:35 +0530

    Upload

---
+++ 05 Bitwise Operator & Loops/05_LeftShift.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 7;
+    int ans = (a << 1);
+
+    cout << "Left Shift of 7 is: " << ans << endl;
+    return 0;
+}
+
+/* Output:
+Left Shift of 7 is: 14
+*/
