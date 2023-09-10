aaf838ea75caeb47db3d69cc938eca14fa485e10
Nitish Singh <nitishkumarsingh909@gmail.com>
2023-09-11 00:11:35 +0530

    Upload

---
+++ 05 Bitwise Operator & Loops/17_LocalVaria.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    for (int i = 0; i < 5; i++)
+    {
+        cout << i << endl;
+    }
+    //cout << i << endl; // Error because It is outside the loop Local variable can't access reason is out of scope.
+    return 0;
+}
---
+++ 05 Bitwise Operator & Loops/18_GlobalVariable.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int a = 5;
+
+int main()
+{
+    for (int i = 0; i < a; i++)
+    {
+        cout << i << endl;
+    }
+    cout << "Value of Global a: " << a;
+    return 0;
+}
+
+/* Output:
+0
+1
+2
+3
+4
+Value of Global a: 5
+*/
