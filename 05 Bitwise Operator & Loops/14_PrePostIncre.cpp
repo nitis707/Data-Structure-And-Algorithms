48355f492817b6558da74977689fbf209aa61398
Nitish Singh <nitishkumarsingh909@gmail.com>
2023-09-10 23:49:27 +0530

    Upload

--- 05 Bitwise Operator & Loops/14_PrePostIncre.cpp
+++ 05 Bitwise Operator & Loops/14_PrePostIncre.cpp
@@ -4,13 +4,13 @@ using namespace std;
 int main()
 {
     int a = 10;
-    
+
     cout << (++a) * (a++) << endl;
     cout <<"a: "<< a << endl;
     return 0;
 }
 
 /* Output:
-132
+132/121 why two types of value in diff compler
 a: 12
 */
