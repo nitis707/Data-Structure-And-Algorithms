e0d55f3885bb4d537d892acb7b88be01db5fc86b
Nitish Singh <nitishkumarsingh909@gmail.com>
2023-09-10 23:27:32 +0530

    Upload

---
+++ 05 Bitwise Operator & Loops/14_PrePostIncre.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 10;
+
+    cout << (++a) * (a++) << endl;
+    cout <<"a: "<< a << endl;
+    return 0;
+}
+
+/* Output:
+132
+a: 12
+*/
