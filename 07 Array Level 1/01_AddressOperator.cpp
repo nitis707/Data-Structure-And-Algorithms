8aa5fa753386b140b012631b2b003fb9864f6bef
Nitish Singh <nitishkumarsingh909@gmail.com>
2023-09-13 12:59:14 +0530

    Upload

---
+++ 07 Array Level 1/01_AddressOperator.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 5;
+    int arr[10];
+
+    cout << "Address of a: " << &a << endl;
+    cout << "Size of a: " << sizeof(a) << " bytes." << endl;
+
+    cout << "Address of arr: " << &arr << endl;
+    cout << "Size of arr: " << sizeof(arr) << " bytes." << endl;
+
+    return 0;
+}
+
+/* Outpu:
+Address of a: 0x61ff0c
+Size of a: 4 bytes.
+Address of arr: 0x61fee4
+Size of arr: 40 bytes.
+*/
