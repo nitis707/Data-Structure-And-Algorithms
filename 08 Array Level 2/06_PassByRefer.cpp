cfe67ac747f7be03600ebf0e45efe136fbe5c5eb
Nitish Singh <nitishkumarsingh909@gmail.com>
2023-09-17 01:21:22 +0530

    Upload

--- 08 Array Level 2/06_PassByRefer.cpp
+++ 08 Array Level 2/06_PassByRefer.cpp
@@ -1,8 +1,23 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
+
+void passByRefer(int &jaadu)
+{
+    jaadu--;
+    cout << "Jaadu: " << jaadu + 10 << endl;
+}
 
 int main()
 {
-    
+    int sundari = 100;
+    sundari--;
+    sundari = sundari - 5;
+    passByRefer(sundari);
+    cout << "Sundari: " << sundari << endl;
     return 0;
 }
+
+/* Output:
+Jaadu: 103
+Sundari: 93
+*/
