836fc6c1ba39ebb6a86e7ae394a61b497161f5c2
Nitish Singh <nitishkumarsingh909@gmail.com>
2023-09-17 02:00:46 +0530

    Upload

--- 08 Array Level 2/08_AllPairs.cpp
+++ 08 Array Level 2/08_AllPairs.cpp
@@ -2,11 +2,32 @@
 Print all pairs for the element present in array.
 */
 
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 int main()
 {
-    
+    int arr[] = {10, 20, 30};
+    int n = 3;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << arr[i] << ", " << arr[j] << endl;
+        }
+    }
     return 0;
 }
+
+/* Output:
+10, 10
+10, 20
+10, 30
+20, 10
+20, 20
+20, 30
+30, 10
+30, 20
+30, 30
+*/
