0718dc56c8f9577e4563c4dc9e78a6f5ddbe6150
Nitish Singh <nitishkumarsingh909@gmail.com>
2023-09-16 18:42:50 +0530

    Upload

--- 08 Array Level 2/03_PassByValue.cpp
+++ 08 Array Level 2/03_PassByValue.cpp
@@ -1,8 +1,23 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
+
+void passByValue(int jaadu)
+{
+    jaadu--;
+    cout << "Jaadu: " << jaadu << endl;
+}
 
 int main()
 {
-    
+    int sundari = 100;
+    sundari--;
+    sundari = sundari - 5;
+    passByValue(sundari);
+    cout << "Sundari: " << sundari << endl;
     return 0;
 }
+
+/* Output:
+Jaadu: 93
+Sundari: 94
+*/
