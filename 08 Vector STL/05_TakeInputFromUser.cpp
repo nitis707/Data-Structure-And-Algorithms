4c9d67b9491accee8b8e6b4246a429e51fd1b8e8
Nitish Singh <nitishkumarsingh909@gmail.com>
2023-10-14 10:13:39 +0530

    Upload

---
+++ 08 Vector STL/05_TakeInputFromUser.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void printVec(vector<int> v)
+{
+    cout << "Print the vector: " << endl;
+    int size = v.size();
+
+    for (int i = 0; i < size; i++)
+    {
+        cout << v[i] << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    vector<int> v;
+
+    // insert
+    int n;
+    cout << "Enter n: ";
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        int d;
+        cin >> d;
+        v.push_back(d);
+    }
+
+    // for push_back more element without asking user
+    for (int i = 0; i < 5; i++)
+    {
+        v.push_back(6);
+    }
+    printVec(v);
+
+    return 0;
+}
+
+/* Output:
+Enter n: 5
+1 2 3 4 5
+Print the vector:
+1 2 3 4 5 6 6 6 6 6
+*/
