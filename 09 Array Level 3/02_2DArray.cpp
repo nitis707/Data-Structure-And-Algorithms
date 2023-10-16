a4fd3d7869e45b92a528183de66ed9cffc2e0cb4
Nitish Singh <nitishkumarsingh909@gmail.com>
2023-10-16 15:12:10 +0530

    Upload

--- 09 Array Level 3/02_2DArray.cpp
+++ 09 Array Level 3/02_2DArray.cpp
@@ -3,11 +3,32 @@ using namespace std;
 
 void printArray(int arr[][4], int row, int col)
 {
-    
+    for (int i = 0; i < col; i++)
+    {
+        for (int j = 0; j < row; j++)
+        {
+            cout << arr[j][i] << " ";
+        }
+        cout << endl;
+    }
 }
 
 int main()
 {
+    int row = 3;
+    int col = 4;
+    int arr[][4] = {
+        {1, 2, 3, 4},
+        {5, 6, 7, 8},
+        {9, 10, 11, 12}};
 
+    printArray(arr, row, col);
     return 0;
 }
+
+/* Output:
+1 5 9 
+2 6 10 
+3 7 11 
+4 8 12 
+*/
