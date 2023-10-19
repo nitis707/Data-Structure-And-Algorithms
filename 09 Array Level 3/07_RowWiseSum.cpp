80278cc348992b29d7448409973f9a025120a7e0
Nitish Singh <nitishkumarsingh909@gmail.com>
2023-10-19 16:28:13 +0530

    Upload

--- 09 Array Level 3/07_RowWiseSum.cpp
+++ 09 Array Level 3/07_RowWiseSum.cpp
@@ -1,8 +1,42 @@
-#include<iostream>
+/*
+Print row-wise sum.
+1 2 3 4
+5 6 7 8
+9 10 1 11
+*/
+
+#include <iostream>
 using namespace std;
+
+void printRowSum(int arr[][4], int row, int col)
+{
+    for (int i = 0; i < row; i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < col; j++)
+        {
+            sum = sum + arr[i][j];
+        }
+        cout << "Sum for row " << i << " is: " << sum << endl;
+    }
+}
 
 int main()
 {
-    
+    int arr[3][4] = {
+        {1, 2, 3, 4},
+        {5, 6, 7, 8},
+        {9, 10, 1, 11}};
+
+    int row = 3;
+    int col = 4;
+
+    printRowSum(arr, row, col);
     return 0;
 }
+
+/* Output:
+Sum for row 0 is: 10
+Sum for row 1 is: 26
+Sum for row 2 is: 31
+*/
--- 09 Array Level 3/08_ColWiseSum.cpp
+++ 09 Array Level 3/08_ColWiseSum.cpp
@@ -1,8 +1,43 @@
-#include<iostream>
+/*
+Print col-wise sum.
+1 2 3 4
+5 6 7 8
+9 10 1 11
+*/
+
+#include <iostream>
 using namespace std;
+
+void printColSum(int arr[][4], int row, int col)
+{
+    for (int i = 0; i < col; i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < row; j++)
+        {
+            sum = sum + arr[j][i];
+        }
+        cout << "Sum for col " << i << " is: " << sum << endl;
+    }
+}
 
 int main()
 {
-    
+    int arr[3][4] = {
+        {1, 2, 3, 4},
+        {5, 6, 7, 8},
+        {9, 10, 1, 11}};
+
+    int row = 3;
+    int col = 4;
+
+    printColSum(arr, row, col);
     return 0;
 }
+
+/* Output:
+Sum for col 0 is: 15
+Sum for col 1 is: 18
+Sum for col 2 is: 11
+Sum for col 3 is: 23
+*/
