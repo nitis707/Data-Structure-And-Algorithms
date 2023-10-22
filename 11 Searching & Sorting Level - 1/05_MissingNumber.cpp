4e9b84a33f64183099e2b1c1fdd86f6f29a8d740
Nitish Singh <nitishkumarsingh909@gmail.com>
2023-10-22 19:04:20 +0530

    Initial commit

---
+++ 11 Searching & Sorting Level - 1/05_MissingNumber.cpp
@@ -0,0 +1,53 @@
+/*
+Program to find missing number in a sorted array.
+Let the range be 1--N, Given input element in array is N-1.
+
+I/P - 1 2 3 4  6 7 8 9
+O/P - Missing element is : 5
+*/
+
+#include <iostream>
+using namespace std;
+
+int findMissingNum(int arr[], int n)
+{
+    int start = 0;
+    int end = n - 1;
+    int mid = start + (end - start) / 2;
+    int ans = -1;
+
+    while (start <= end)
+    {
+        int diff = arr[mid] - mid;
+
+        if (diff == 1)
+        {
+            // go to right
+            start = mid + 1;
+        }
+        else
+        {
+            // ans store
+            ans = mid;
+            // got to left
+            end = mid - 1;
+        }
+        mid = start + (end - start) / 2;
+    }
+    return ans + 1;
+}
+
+int main()
+{
+    int arr[] = {1, 2, 3, 4, 6, 7, 9};
+    int n = 8;
+
+    int ans = findMissingNum(arr, n);
+    cout << "Missing element is: " << ans << endl;
+
+    return 0;
+}
+
+/* Output:
+Missing element is: 5
+*/
